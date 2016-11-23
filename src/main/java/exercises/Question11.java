536b5d9224a0cc841152f7260017d5268c360e89
Adrian Herscu <adrian.herscu@gmail.com>
2016-11-23 19:18:10 +0200

    added exercise

---
+++ src/main/java/exercises/Question11.java
@@ -0,0 +1,26 @@
+package exercises;
+
+/**
+ * Created by adrian on 11/22/2016.
+ */
+public class Question11 {
+    public static boolean hasUniqueChars(final String s) {
+        boolean[] charOccurs = new boolean[Character.MAX_VALUE];
+
+        for(final Character c : s.toCharArray()) {
+            if(charOccurs[c]) return false;
+            charOccurs[c] = true;
+        }
+
+        return true;
+    }
+    public static boolean hasUniqueChars2(final String s) {
+        for(int i = 0; i < s.length(); i++) {
+            for(int j = i+1; j < s.length(); j++) {
+                if(s.charAt(i)==s.charAt(j)) return false;
+            }
+        }
+
+        return true;
+    }
+}
---
+++ src/test/java/exercises/Question11Test.java
@@ -0,0 +1,31 @@
+package exercises;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.assertFalse;
+import static org.testng.Assert.assertTrue;
+
+/**
+ * Created by adrian on 11/22/2016.
+ */
+public class Question11Test {
+    @Test
+    public void hasUniqueChars() throws Exception {
+        assertTrue(Question11.hasUniqueChars("abc"));
+    }
+
+    @Test
+    public void notHasUniqueChars() throws Exception {
+        assertFalse(Question11.hasUniqueChars("abbc"));
+    }
+
+    @Test
+    public void hasUniqueChars2() throws Exception {
+        assertTrue(Question11.hasUniqueChars2("abc"));
+    }
+
+    @Test
+    public void notHasUniqueChars2() throws Exception {
+        assertFalse(Question11.hasUniqueChars2("abebc"));
+    }
+}
