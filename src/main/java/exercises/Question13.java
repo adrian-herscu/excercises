a7fef5d7a5d2753bd4b0c89f29eb882422ebb9a4
Adrian Herscu <adrian.herscu@gmail.com>
2016-11-23 22:32:30 +0200

    added exercise

---
+++ src/main/java/exercises/Question13.java
@@ -0,0 +1,10 @@
+package exercises;
+
+/**
+ * Created by adrian on 11/23/2016.
+ */
+public class Question13 {
+    public static final char[] dedup(final char[] string) {
+        return string;
+    }
+}
---
+++ src/test/java/exercises/Question13Test.java
@@ -0,0 +1,16 @@
+package exercises;
+
+import org.testng.annotations.Test;
+
+import static org.testng.Assert.*;
+
+/**
+ * Created by adrian on 11/23/2016.
+ */
+public class Question13Test {
+    @Test
+    public void testDedup() {
+
+    }
+
+}
