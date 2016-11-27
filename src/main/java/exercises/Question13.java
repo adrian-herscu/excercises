2e18411d8da6692e7694ae9c0cb6ea5371f49751
Adrian Herscu <adrian.herscu@gmail.com>
2016-11-27 20:24:03 +0200

    in progress

--- src/main/java/exercises/Question13.java
+++ src/main/java/exercises/Question13.java
@@ -1,10 +1,21 @@
 package exercises;
 
 /**
- * Created by adrian on 11/23/2016.
+ * @author adrian
  */
 public class Question13 {
     public static final char[] dedup(final char[] string) {
+        final boolean[] charOccurs = new boolean[Character.MAX_VALUE];
+
+        for(int index = 0, dedupedIndex = 0; index < string.length; index++) {
+            if(!charOccurs[string[index]]) {
+                charOccurs[string[index]] = true;
+                dedupedIndex = index;
+            } else {
+                string[dedupedIndex++] = string[index];
+            }
+        }
+
         return string;
     }
 }
--- src/test/java/exercises/Question13Test.java
+++ src/test/java/exercises/Question13Test.java
@@ -1,16 +1,30 @@
 package exercises;
 
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-import static org.testng.Assert.*;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
 
 /**
- * Created by adrian on 11/23/2016.
+ * @author adrian
  */
 public class Question13Test {
-    @Test
-    public void testDedup() {
-
+    @DataProvider
+    private Object[][] data() {
+        return new Object[][]{
+                {new char[]{}, new char[]{}},
+                {new char[]{'a', 'b', 'c', 'b'}, new char[]{'a', 'b', 'c', 0}},
+                {new char[]{'f', 'b', 'c', 'b', 'c','j'}, new char[]{'f', 'b', 'c', 'j', 0, 0}}
+        };
     }
 
+    @Test(dataProvider = "data")
+    public void testDedup(
+            final char[] string,
+            final char[] expected) {
+        assertThat(
+                Question13.dedup(string),
+                equalTo(expected));
+    }
 }
