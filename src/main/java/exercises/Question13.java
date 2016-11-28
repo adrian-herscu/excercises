a596c5b88b7fe630456c84afa6c6f67fde6899dc
aherscu <aherscu@ebay.com>
2016-11-28 09:37:59 +0200

    completed question 1.3

--- src/main/java/exercises/Question13.java
+++ src/main/java/exercises/Question13.java
@@ -7,15 +7,25 @@ public class Question13 {
     public static final char[] dedup(final char[] string) {
         final boolean[] charOccurs = new boolean[Character.MAX_VALUE];
 
-        for(int index = 0, dedupedIndex = 0; index < string.length; index++) {
-            if(!charOccurs[string[index]]) {
-                charOccurs[string[index]] = true;
-                dedupedIndex = index;
+        for(int index = 0; index < string.length; index++) {
+            if(charOccurs[string[index]]) {
+                shiftLeft(string, index--);
             } else {
-                string[dedupedIndex++] = string[index];
+                charOccurs[string[index]] = true;
             }
         }
 
         return string;
+    }
+    
+    private static final void shiftLeft(
+        final char[] string,
+        final int fromIndex) {
+        
+        for(int index = fromIndex + 1; index < string.length; index++) {
+            string[index - 1] = string[index];
+        }
+        
+        string[string.length-1] = 0;
     }
 }
--- src/test/java/exercises/Question13Test.java
+++ src/test/java/exercises/Question13Test.java
@@ -12,19 +12,23 @@ import static org.hamcrest.Matchers.equalTo;
 public class Question13Test {
     @DataProvider
     private Object[][] data() {
-        return new Object[][]{
-                {new char[]{}, new char[]{}},
-                {new char[]{'a', 'b', 'c', 'b'}, new char[]{'a', 'b', 'c', 0}},
-                {new char[]{'f', 'b', 'c', 'b', 'c','j'}, new char[]{'f', 'b', 'c', 'j', 0, 0}}
+        return new Object[][] {
+            { new char[] {}, new char[] {} },
+            {
+                new char[] { 'a', 'b', 'c', 'b' },
+                new char[] { 'a', 'b', 'c', 0 } },
+            {
+                new char[] { 'f', 'b', 'c', 'b', 'c', 'j' },
+                new char[] { 'f', 'b', 'c', 'j', 0, 0 } }
         };
     }
 
     @Test(dataProvider = "data")
     public void testDedup(
-            final char[] string,
-            final char[] expected) {
+        final char[] string,
+        final char[] expected) {
         assertThat(
-                Question13.dedup(string),
-                equalTo(expected));
+            Question13.dedup(string),
+            equalTo(expected));
     }
 }
